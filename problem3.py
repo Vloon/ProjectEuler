794a23881d58850e9eb8740bd0819c68398fa36c
Vloon <pleunscholten@hotmail.com>
2019-03-21 19:57:00 +0100

    Added problem3

---
+++ problem3.py
@@ -0,0 +1,21 @@
+def get_primes(nr_primes):
+    primes = []
+    for n in range(2,nr_primes):
+        is_divisible = False
+        for p in primes:
+            if n%p == 0:
+                is_divisible = True
+        if not is_divisible:
+            primes.append(n)
+    return primes
+
+primes = get_primes(10000)
+target = 600851475143
+i = 0
+while target >= primes[i]:
+    if target%primes[i] == 0:
+        print(primes[i],target)
+        target = target/primes[i]
+    i += 1
+
+### This code is fugly
